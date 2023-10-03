069a2e687663f085629c7d179dd928005b931f3a
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-10-03 11:48:39 +0200

    scripts/check-remark-root.sh: print line number
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/check-remark-root.sh
+++ scripts/check-remark-root.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-grep -E '"remark|"images|\(images|\(img|"img' "$1"
+grep -En '"remark|"images|\(images|\(img|"img' "$1"
 
 GREP_EC=$?
 
 if [ $GREP_EC -eq 0 ]; then
   echo "Found problematic lines in: $1"
+  echo "See above for line numbers"
   exit 1
 else
   echo "No problematic lines found in: $1"
